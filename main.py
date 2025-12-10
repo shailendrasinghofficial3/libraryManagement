809f6b31c3859d524df7a0b5ab46271cbc2f33d0
shailendrasinghofficial3 <shailendrasinghofficial3@gmail.com>
2025-12-10 11:33:23 +0530

    post request using Body(...)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI, UploadFile
+from fastapi.params import Body
 
 app = FastAPI()
 @app.get("/")
@@ -12,3 +14,10 @@ async def greet_user(name:str)->dict:
 @app.get("/greet/{name}")
 async def greet_user(name:str,age:int)->dict:
     return {"message":f"Hello {name}","age":age}
+#using Body(...),mostly used when there is single field or file upload,
+# File inputs use File(...) and UploadFile, not models:
+@app.post("/upload")
+async def  uploadData(file:UploadFile,metadata:str=Body(...))->dict:
+    metadata_dict = json.loads(metadata)
+    return {"new_upload":f"uploaded file {file.filename},name is{metadata}"}
+#usimgpydantic
